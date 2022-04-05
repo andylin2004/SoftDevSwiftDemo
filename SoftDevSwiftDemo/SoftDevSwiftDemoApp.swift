fa76647efa8990f71a1713d988929bba3afe8180
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-04-05 09:04:48 -0400

    Initial Commit

---
+++ SoftDevSwiftDemo/SoftDevSwiftDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoftDevSwiftDemoApp.swift
+//  SoftDevSwiftDemo
+//
+//  Created by Andy Lin on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SoftDevSwiftDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
